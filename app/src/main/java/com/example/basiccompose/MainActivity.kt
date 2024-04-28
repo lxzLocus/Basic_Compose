9bb04e23eab8cbe27b232a81388dcf6a1815d957
lxzLocus <unicorngundam03@gmail.com>
2024-04-29 04:52:03 +0900

    Jetpack Compose tutorial 完

--- app/src/main/java/com/example/basiccompose/MainActivity.kt
+++ app/src/main/java/com/example/basiccompose/MainActivity.kt
@@ -3,13 +3,24 @@ package com.example.basiccompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.basiccompose.ui.theme.BasicComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +33,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    textCompose(
+                        stringResource(R.string.text_1),
+                        text_2 = stringResource(R.string.text_2),
+                        text_3 = stringResource(R.string.text_3)
+                    )
                 }
             }
         }
@@ -30,17 +45,51 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun textCompose (text_1: String, text_2: String, text_3: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.bg_compose_background)
+
+    Column{
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+
+        Text(
+            text = text_1,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+        Text(
+            text = text_2,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+        Text(
+            text = text_3,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+    }
+
 }
 
-@Preview(showBackground = true)
+
+
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+    )
 @Composable
 fun GreetingPreview() {
     BasicComposeTheme {
-        Greeting("Android")
+        textCompose(
+            stringResource(R.string.text_1),
+            text_2 = stringResource(R.string.text_2),
+            text_3 = stringResource(R.string.text_3)
+        )
     }
 }
